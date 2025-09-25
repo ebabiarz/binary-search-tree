dfeb89facc7c2e169d1a73b2c8f0ac4b8262eb69
ebabiarz <145168964+ebabiarz@users.noreply.github.com>
2025-09-24 20:52:39 -0500

    Create Node class.

---
+++ lib/node.rb
@@ -0,0 +1,14 @@
+class Node
+  include Comparable
+  attr_accessor :data, :right, :left
+
+  def initialize(data, right = nil, left = nil)
+    @data = data
+    @right = right
+    @left = left
+  end
+
+  def <=>(other_node)
+    data <=> other_node.data
+  end
+end
